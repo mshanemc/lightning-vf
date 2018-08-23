d3c53328f1874c31fbf3b85893c315b8a92bf1cc
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-23 10:08:52 -0500

    initial commit plus sample data loading

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,9 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:package:install -r --package 04t6A000000SG0F -w 20
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:apex:execute -f scripts/dataWipe.cls
+sfdx force:data:tree:import -p data/sample-data-plan.json
+
 sfdx force:source:push
+
 sfdx force:org:open
