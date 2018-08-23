b6344325c796f19fec69b88cebb870624621b44a
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-23 10:27:23 -0500

    creates username/password

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,12 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+# sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json -u vf -o work.shop
+
 sfdx force:package:install -r --package 04t6A000000SG0F -w 20
 
 sfdx force:apex:execute -f scripts/dataWipe.cls
 sfdx force:data:tree:import -p data/sample-data-plan.json
 
 sfdx force:source:push
+sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
 
 sfdx force:org:open
